bda3884570cb119298bd98361e7669a2a58df6d6
mkeute <marius.keute@gmail.com>
2020-12-14 19:07:39 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+ 
+setup(
+   name='remotexdf',    # This is the name of your PyPI-package.
+   packages = ['remotexdf']                  # The name of your scipt, and also the command you'll be using for calling it
+)
